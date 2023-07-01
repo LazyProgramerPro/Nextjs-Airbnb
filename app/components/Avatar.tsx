a9dc95aede663b119aa7cc84de77a1c209456065
[BK] Anh Túc <husthunter97@gmail.com>
2023-07-01 09:13:39 +0700

    [AIRBNB][BK Hải Đường] add categories

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,14 +1,19 @@
 'use client';
 
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 interface AvatarProps {
   src: string | null | undefined;
 }
 
 const Avatar:React.FC<AvatarProps> = ({src}) => {
+
+  const router = useRouter()
+
   return (
     <Image
+      onClick={() => router.push('/')}
       className="rounded-full"
       alt="Avatar"
       width="30"
